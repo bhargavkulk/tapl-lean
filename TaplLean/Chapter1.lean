c02497965e56386eb6f3ec5c5cc8d330fc3915d0
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-07-01 15:05:04 -0600

    save

--- TaplLean/Chapter1.lean
+++ TaplLean/Chapter1.lean
@@ -234,7 +234,7 @@ instance : StepRel Term where
   step := BStep
 
 /- Theorem 3.5.4 [Determinancy of One-Step Evaluation] -/
-theorem step_deterministic {t t' t'' : Term} (H1 : t -> t') (H2 : t -> t'')
+theorem step_is_deterministic {t t' t'' : Term} (H1 : t -> t') (H2 : t -> t'')
   : t' = t'' := by
   induction H1 generalizing t'' with
   | EIfTrue =>
@@ -297,31 +297,97 @@ theorem normal_forms_are_values {t} (Hv : ¬ Value t) : ¬ NormalForm t := by
     apply BStep.EIf
     assumption
 
-inductive MultiStep : Term → Term → Prop where
-| refl {t} : MultiStep t t
-| step (t t' : Term) (h : t -> t') : MultiStep t t'
-| trans (t t' t'' : Term) (h₁ : MultiStep t t') (h₂ : MultiStep t' t'') : MultiStep t t''
+inductive MultiStep : Term → Term → Prop
+| refl (x : Term) : MultiStep x x
+| step (x y z : Term) (h : BStep x y) (h' : MultiStep y z) : MultiStep x z
 
 infixr:50 " ->* " => MultiStep
 
+
+lemma multi_step (x y : Term) (H: x -> y) : x ->* y := by
+  apply MultiStep.step (y := y)
+  · assumption
+  · apply MultiStep.refl
+
+lemma multi_trans (x y z: Term) (H1: x ->* y) (H2: y ->* z) : x ->* z := by
+  induction H1 with
+  | refl x => assumption
+  | step x' y' z' H H' IH =>
+    have IH := IH H2
+    apply MultiStep.step (y := y') <;> assumption
+
 /- 3.5.11 Theorem [Uniqueness of normal forms]: If t -→∗ u and t -→∗ u′, where u
 and u′ are both normal forms, then u = u′. -/
+theorem uniqueness_of_normal_forms {x y1 y2 : Term}
+  (H : x ->* y1)
+  (H' : x ->* y2)
+  (Hn : NormalForm y1)
+  (Hn' : NormalForm y2) :
+  y1 = y2 := by
+  revert y2 -- copied from software foundations kek
+  induction H with
+  | refl x =>
+    intro y2 H Hnf
+    cases H with
+    | refl => rfl
+    | step x' y' z' => -- contradiction here somehow
+      exfalso
+      apply Hn
+      exists y'
+  | step x' y' z' H H' IH =>
+    intro y2 Hs Hn'
+    cases Hs with
+    | refl => -- contradiction here too
+      exfalso
+      apply Hn'
+      exists y'
+    | step x'' y'' z'' =>
+      apply IH -- no clue what happened here, vibes
+      · assumption
+      · have Heq: y' = y'' := by
+          apply step_is_deterministic (t := x') <;> assumption
+        simp [Heq]
+        assumption
+      · assumption
 
-theorem uniqueness_of_normal_forms {t u u' : Term}
-  (H : t ->* u)
-  (H' : t ->* u')
-  (Hn : NormalForm u)
-  (Hn' : NormalForm u') :
-  u = u' := sorry
+/- 3.5.12 Theorem [Termination of Evaluation]:
+  For every term t there is some normal form t′ such that t →∗ t′.  -/
+-- might need some auxilary lemmas
+/- lemma multistep_congr_cond :
+    ∀ t1 t1' t2 t3,
+    t1 ->* t1' →
+    Term.ite t1 t2 t3 ->* Term.ite t1' t2 t3 := by
+  intro t1 t1' t2 t3 H
+  induction H with
+  | refl => apply MultiStep.refl
+  | step t t' t'' H H' IH =>
+    apply MultiStep.step (y := t'.ite t2 t3)
+    · apply BStep.EIf
+      assumption
+    · assumption -/
 
-
-
-
-
-
-
-
-
-
+-- not sure how to prove this, or whether our semantics allow this
+theorem termination_of_evaluation :
+  ∀ t, ∃ t', t ->* t' ∧ NormalForm t' := by
+  intro t
+  induction t with
+  -- true / false are trivially true because they are already
+  -- normal forms
+  | true =>
+    exists Term.true
+    constructor
+    · apply MultiStep.refl
+    · simp [NormalForm]
+      intro x H
+      cases H
+  | false =>
+    exists Term.false
+    constructor
+    · apply MultiStep.refl
+    · simp [NormalForm]
+      intro x H
+      cases H
+  -- trickier case
+  | ite t1 t2 t3 IH1 IH2 IH3 => sorry
 
 end UBool
--- TaplLean/Util.lean
+++ TaplLean/Util.lean
@@ -6,10 +6,3 @@ class StepRel (α : Type u) where
   step : α → α → Prop
 
 infixr:50 " -> " => StepRel.step
-
-inductive MultiStep {α : Type u} [inst : StepRel α] : α → α → Prop where
-  | refl {t} : MultiStep t t
-  | step {t t'} (h : StepRel.step t t') : MultiStep t t'
-  | trans {t t' t''} (h₁ : MultiStep t t') (h₂ : MultiStep t' t'') : MultiStep t t''
-
-infixr:50 " ->* " => MultiStep
